9fa84b6b9c57091709b18d3b0d2e0e5ac6d6cddc
Jhon <yc12312@hanmail.net>
2018-10-02 18:40:18 +0900

    Add function to log in

---
+++ 2D_Game_Seer/Assets/Script/AuthManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AuthManager : MonoBehaviour {
+
+    AuthUI authUI;
+
+    FirebaseAuth auth;
+
+	// Use this for initialization
+	void Start () {
+        authUI = GetComponent<AuthUI>();
+
+        InitializeFirebase();
+	}
+
+    void InitializeFirebase()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+    }
+
+
+    public void OnLoginButtonClick(){
+        TryLoginWithFirebaseAuth(authUI.SigninEmail.text, authUI.SigninPassword.text);
+    }
+
+    public void OnCreateaccountButtonClick()
+    {
+        TrySignUpWithFirebaseAuth(authUI.SigninEmail.text, authUI.SigninPassword.text, authUI.SigninConfirmPassword.text);
+    }
+
+    public void OnSignoutButtonClick()
+    {
+        auth.SignOut();
+        SceneManager.LoadScene("Log_In");
+    }
+
+    private void TryLoginWithFirebaseAuth(string email, string password){
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(obj => {
+
+            authUI.ShowLoggedinPanel();
+        });
+    }
+
+    //check validity of pw and email
+    private void TrySignUpWithFirebaseAuth(string email, string password,string confirmpassword){
+        
+        if(password != confirmpassword){
+            Debug.Log("pw not matched");
+            return;
+        }
+
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(obj => {
+
+            authUI.ShowLoginPanel();
+        });
+    }
+}
---
+++ 2D_Game_Seer/Assets/Script/AuthUI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class AuthUI : MonoBehaviour {
+
+    public GameObject LoginPanel;
+    public GameObject SignupPanel;
+    public GameObject LoggedinPanel;
+
+    public InputField loginEmail;
+    public InputField loginPassword;
+
+    public InputField SigninEmail;
+    public InputField SigninPassword;
+    public InputField SigninConfirmPassword;
+
+
+	// Use this for initialization
+	void Start () {
+        
+        ShowLoginPanel();
+	}
+	
+    public void ShowLoginPanel()
+    {
+        ShowPanel(LoginPanel);
+    }
+    public void ShowSignupPanel()
+    {
+        ShowPanel(SignupPanel);
+
+    }
+    public void ShowLoggedinPanel()
+    {
+        SceneManager.LoadScene("Main_menu_1");
+    }
+
+    public void ShowPanel(GameObject panel)
+    {
+        LoginPanel.SetActive(false);
+        SignupPanel.SetActive(false);
+
+        panel.SetActive(true);
+    }
+}
